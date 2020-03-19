e76b6d72c30780f5c73a923b563bfda937fd055f
Immanuel Paul Perli <immanuel.perli@servicenow.com>
2020-03-19 14:03:49 -0700

    first commit

---
+++ src/test/java/junitreportgeneration/JUnitReportGeneration2Test.java
@@ -0,0 +1,31 @@
+package junitreportgeneration;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class JUnitReportGeneration2Test {
+
+	private String developer = "Vinod";
+
+	@Test
+	public void instanceOfTest1() {
+		assertThat(new ArrayList<>(), instanceOf(List.class));
+	}
+
+	@Test
+	public void assertTrueTest2() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void equalToTest2() {
+		assertThat(developer, is("Paul"));
+	}
+}
